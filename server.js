8360bed783c61289fe997bd107560e91ead32b45
MattewMakar <mattewmakar@gmail.com>
2020-10-11 09:29:03 -0400

    Changing GET

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ Handlebars.registerHelper("checkImage", () => {
 //===============================================================================
 //GET method with any url will lead to the landing page
 //===============================================================================
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   m_data.focus = "Gallery";
   res.render('index', {data : m_data});
 });
